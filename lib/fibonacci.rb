5096de83f0854e203c05219bd24db490e1ffe4f0
Dikla Aharoni Rikovitch <dikla.aharoni@gmail.com>
2018-03-24 13:30:00 -0700

    finished fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,19 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  return 1 if n == 1
+  return 0 if n == 0
+  if n == nil || n < 0
+    raise ArgumentError.new("nil")
+  end
+
+  num1 = 0
+  num2 = 1
+  fibonacci_num = 0
+  (n - 1).times do
+    fibonacci_num = (num1 + num2)
+    num1 = num2
+    num2 = fibonacci_num
+  end
+  return fibonacci_num
 end
