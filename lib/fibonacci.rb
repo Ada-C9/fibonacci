2024357188b039d444cdd6a49e44438a597ec84c
Zheng Cao <chori84@gmail.com>
2018-03-26 08:06:07 -0700

    Implement fibonacci and pass tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,8 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  raise ArgumentError if n.nil? || n < 0
+  return 0 if n == 0
+  return 1 if n == 1
+  return fibonacci(n - 2) + fibonacci(n - 1) if n >= 2
 end
