1c380b24c7750bcf1caa844e9e539f96e49f4e5e
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-05 22:01:10 -0700

    Write functionality for fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,32 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  if n == nil
+    raise ArgumentError.new('Cannot enter nil value.')
+  elsif n < 0
+    raise ArgumentError.new('Must enter a number greater than or equal to zero.')
+  end
+
+  fib = 0
+  fib_zero = 0
+  fib_one = 1
+
+  if n == 0
+    fib = fib_zero
+  elsif n == 1
+    fib = fib_one
+  elsif n > 1
+    fib_prev = fib_zero + fib_one
+    fib_next_prev = fib_one
+    fib = fib_prev
+    position = 2
+    while position < n
+      fib = fib_prev + fib_next_prev
+      fib_next_prev = fib_prev
+      fib_prev = fib
+      position += 1
+    end
+  end
+
+  return fib
 end
