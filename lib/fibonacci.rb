8339942dfd68ece7442fc587b9acaf3c816a7a57
Steffany Brown <steffannbrown@gmail.com>
2018-04-06 13:51:59 -0700

    tests passing

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,31 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+
+  if n == nil || n < 0
+    raise ArgumentError
+  end
+
+  if n == 0
+    return 0
+  end
+
+  if n == 1
+    return 1
+  end
+
+  begin_num = 0
+  next_num = 1
+  number = 0
+
+  iterations = n - 1
+
+  iterations.times do
+    number = begin_num + next_num
+    begin_num = next_num
+    next_num = number
+  end
+
+  return number
+
 end
--- specs/fibonacci_spec.rb
+++ specs/fibonacci_spec.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/fibonacci'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "fibonacci" do
   describe "basic tests" do
