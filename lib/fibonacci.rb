37d918845172b1b467b6ca7291885aef4886b425
Maja Graonic <m.graonic@gmail.com>
2018-04-07 16:33:03 -0700

    tests passing

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,40 @@
 # Computes the nth fibonacci number in the series starting with 0.
-# fibonacci series: 0 1 1 2 3 5 8 13 21 ...
-# e.g. 0th fibonacci number is 0
-# e.g. 1st fibonacci number is 1
-# ....
-# e.g. 6th fibonacci number is 8
+
+# Recursive solution has linear space complexity or O(n): memory required is proportional to the number of function calls to f(n-1)
+# Recursive solution has factorial time complexity or O(2^n): the number of function calls (ie, operations) nearly doubles with each increase in input
+
+# Iterative solution has constant space complexity or O(1): the amount of memory taken is constant regardless of input size
+# Iterative solution has linear time complexity or O(n): the number of operations (ie, runs of the loop) is as long as the unput
+
 def fibonacci(n)
-  raise NotImplementedError
+  if n == nil || n < 0
+    raise ArgumentError
+  end
+# base cases are 0 and 1 (0th place is 0, 1st place is 1)
+  if n < 2
+    return n
+  end
+
+  counter = 1
+  left = 0
+  right = 1
+
+  while counter < n
+    sum = left + right
+    left = right
+    right = sum
+    counter += 1
+  end
+  return sum
 end
+
+# def fibonacci(n)
+# if n == nil || n < 0
+#   raise ArgumentError
+# end
+# # base cases are 0 and 1 (0th place is 0, 1st place is 1)
+#   if n < 2
+#     return n
+#   end
+#   return fibonacci(n - 1) + fibonacci(n - 2)
+# end
