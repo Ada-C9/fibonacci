a26f4e66b93f1357fac19a231bff8256c77c7adf
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-07 22:21:45 -0700

    add fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,21 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+
+  if n == 0 || n == 1
+    fib_num = n
+  elsif n == nil || n < 0
+    raise ArgumentError
+  else
+    fib_num = 0
+    last_num = 1
+    twice_last_num = 0
+    n -= 1
+    n.times do |i|
+      fib_num = last_num + twice_last_num
+      twice_last_num = last_num
+      last_num = fib_num
+    end
+  end
+  return fib_num
 end
