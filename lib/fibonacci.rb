46e0b75ff934a7fe863f30ccd5ffd2a829c9810c
knockknockhusthere <angela.huxia@gmail.com>
2018-04-08 14:25:54 -0700

    fibonacci sequence

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,39 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
+  a = 0
+  b = 1
+  raise ArgumentError if n == nil || n < 0
+  return 0 if n == 0
+  return 1 if n == 1
+
+  fib_num = 0
+  (n-1).times do
+    fib_num = a + b
+    a = b
+    b = fib_num
+  end
+
+  return fib_num
+  raise NotImplementedError
+end
+
+def fibonacci(n)
+  raise ArgumentError if n == nil || n < 0
+
+  a = 0
+  b = 1
+  fib_num = 1
+
+  return 0 if n == 0
+
+  fib_num = 1
+  (n-1).times do
+    fib_num = a + b
+    a = b
+    b = fib_num
+  end
+
+  return fib_num
   raise NotImplementedError
 end
