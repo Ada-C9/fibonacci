ac153f067d6da32a12d0dd45c5cdc87045b57ee1
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-07 22:36:35 -0700

    trimmed finonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,21 +5,11 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-
   if n == 0 || n == 1
     fib_num = n
   elsif n == nil || n < 0
     raise ArgumentError
   else
-    fib_num = 0
-    last_num = 1
-    twice_last_num = 0
-    n -= 1
-    n.times do |i|
-      fib_num = last_num + twice_last_num
-      twice_last_num = last_num
-      last_num = fib_num
-    end
+   fibonacci(n-1) + fibonacci(n-2)
   end
-  return fib_num
 end
