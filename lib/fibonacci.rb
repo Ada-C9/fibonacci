c51f2c08454e609bc5529085325e529be31673d0
Abiaina <abinnet.a@gmail.com>
2018-04-08 19:43:14 -0700

    determines nth + integer in fibonacci sequence. raises argument error for all non + integer values

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,17 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  array = [0, 1]
+
+  if n == nil || n < 0
+    raise ArgumentError.new
+  elsif n < 3 && n >= 0
+    return n
+  end
+
+  (n - 1).times do
+    array << (array[(array.length - 1)] + array[(array.length - 2)])
+  end
+
+  return array[(array.length - 1)]
 end
