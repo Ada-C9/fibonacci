f74a1b445ef34108fee88c3a66cc5eb26fd036a2
Samantha Berk <sberk10@gmail.com>
2018-04-14 17:18:50 -0700

    First algorithm passing tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,34 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  # input: int n      e.g. 6
+  # output: the nth fibonacci number    e.g. 8
+
+  # variables
+    # count: keep track of iterations until we reach n
+    # first_num
+    # second_num
+    # result: nth fibonacci (return)
+
+  first_num = 0
+  second_num = 1
+  result = 0
+
+  if n == nil || n < 0
+    raise ArgumentError.new("Must be an integer greater than 0.")
+  elsif n == first_num
+    return first_num
+  elsif n == second_num
+    return second_num
+  end
+
+  count = 2
+  while count <= n # 6<=6
+    result = first_num + second_num  # 3+5=8
+    first_num = second_num #5
+    second_num = result #8
+    count += 1 #7
+  end
+  return result
+
 end
