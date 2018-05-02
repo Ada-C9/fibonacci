f266655bafa055ba6baa3c1c7617d73969cc1391
shrutivanw <shruti@adadevelopersacademy.org>
2018-05-02 16:27:19 -0700

    Update fibonacci.rb
    Updating solution in solution's branch

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,6 +4,24 @@
 # e.g. 1st fibonacci number is 1
 # ....
 # e.g. 6th fibonacci number is 8
+# Time complexity: O(n) - that's the number of times the loop will get executed
+# Space complexity: O(1) - first, second and current use constant space as input value changes
 def fibonacci(n)
-  raise NotImplementedError
+  raise ArgumentError.new("input cannot be nil") if n == nil
+  raise ArgumentError.new("input cannot be negative") if n < 0
+
+  if n == 0 || n == 1
+    return n
+  end
+
+  first = 0
+  second = 1
+  current = 1
+  while n > 2
+    first = second
+    second = current
+    current = first + second
+    n -= 1
+  end
+  return current
 end
